ccdfa502426fbc6a1fe484f5bb34066d095681d5
Nika <variablenames@gmail.com>
2023-02-24 12:45:36 +0400

    Fix aws-ecs module variable

--- modules/aws-ecs/main.tf
+++ modules/aws-ecs/main.tf
@@ -17,7 +17,7 @@ resource "aws_ecs_cluster" "this" {
 resource "aws_ecs_cluster_capacity_providers" "this" {
   count = var.create_ecs ? 1 : 0
 
-  cluster_name = aws_ecs_cluster.this.*.name
+  cluster_name = one(aws_ecs_cluster.this[*].name)
   capacity_providers = var.capacity_providers
 
   dynamic default_capacity_provider_strategy {
